02d3c40fecbc45403a50b0b1996f0f8c2bf04a31
bogdanpasterak <bogdanpasterak@gmail.com>
2019-10-05 21:44:16 +0100

    first step new barnch

--- src/main/java/com/example/demo/TowerOfHanoiApplication.java
+++ src/main/java/com/example/demo/TowerOfHanoiApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+//import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TowerOfHanoiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TowerOfHanoiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(TowerOfHanoiApplication.class, args);
+		TowerOfHanoi towerOfHanoi = (TowerOfHanoi) ctx.getBean("towerOfHanoi");
+		towerOfHanoi.say();
 	}
 
 }
